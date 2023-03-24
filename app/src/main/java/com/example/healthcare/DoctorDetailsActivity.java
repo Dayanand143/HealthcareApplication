83de4965dd5dfe2a20618d0531ba11d3368f820f
Dayanand Gavas <dayanand.gavas143@gmail.com>
2023-03-24 08:31:08 +0530

    Lab test or details activity and layout for the same is added. 2.59

--- app/src/main/java/com/example/healthcare/DoctorDetailsActivity.java
+++ app/src/main/java/com/example/healthcare/DoctorDetailsActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -70,7 +69,7 @@ public class DoctorDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_doctor_details);
 
         tv = findViewById(R.id.textViewDDTitle);
-        btn = findViewById(R.id.buttonDDBack);
+        btn = findViewById(R.id.buttonLDBack);
 
         Intent it = getIntent();
         String title = it.getStringExtra("title");
@@ -113,7 +112,7 @@ public class DoctorDetailsActivity extends AppCompatActivity {
                 new String[]{"line1","line2","line3","line4","line5"},
                 new int[]{R.id.line_a,R.id.line_b,R.id.line_c,R.id.line_d,R.id.line_e}
                 );
-        ListView lst = findViewById(R.id.listViewDD);
+        ListView lst = findViewById(R.id.listViewLT);
         lst.setAdapter(sa);
 
         lst.setOnItemClickListener(new AdapterView.OnItemClickListener() {
