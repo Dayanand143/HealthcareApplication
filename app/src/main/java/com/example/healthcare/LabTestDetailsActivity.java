e5db906bc2783818562696da81ab2f3ebd30c090
Dayanand Gavas <dayanand.gavas143@gmail.com>
2023-03-24 12:15:05 +0530

    Cart, test lab and database updates. 3.21

--- app/src/main/java/com/example/healthcare/LabTestDetailsActivity.java
+++ app/src/main/java/com/example/healthcare/LabTestDetailsActivity.java
@@ -3,12 +3,16 @@ package com.example.healthcare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LabTestDetailsActivity extends AppCompatActivity {
     TextView tvPackageName, tvTotalCost;
@@ -24,6 +28,9 @@ public class LabTestDetailsActivity extends AppCompatActivity {
         tvPackageName = findViewById(R.id.textViewLDPackageName);
         tvTotalCost = findViewById(R.id.textViewLDTotalCost);
         edDetails = findViewById(R.id.editTextLDTextMultiLine);
+        btnAddToCart = findViewById(R.id.buttonLDAddToCart);
+        btnBack = findViewById(R.id.buttonLDBack);
+
         edDetails.setKeyListener(null);
 
         Intent intent=getIntent();
@@ -37,5 +44,26 @@ public class LabTestDetailsActivity extends AppCompatActivity {
                 startActivity(new Intent(LabTestDetailsActivity.this,LabTestActivity.class));
             }
         });
+
+        btnAddToCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+                String username = sharedPreferences.getString("username","").toString();
+                String product = tvPackageName.getText().toString();
+                float price = Float.parseFloat(intent.getStringExtra("text3").toString());
+
+                Database db = new Database(getApplicationContext(),"healthcare",null,1);
+
+                if(db.checkCart(username,product)==1){
+                    Toast.makeText(getApplicationContext(),"Product Already added",Toast.LENGTH_SHORT).show();
+                }else{
+                    db.addCart(username,product,price,"lab");
+                    Toast.makeText(getApplicationContext(),"Record Inserted to Cart",Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(LabTestDetailsActivity.this,LabTestActivity.class));
+                }
+            }
+        });
+
     }
 }
